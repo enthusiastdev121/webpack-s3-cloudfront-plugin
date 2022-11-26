7906f69676a0ca4acad9cfaa5917e1cf79cece93
DonGroovy1120 <groovyrocome@gmail.com>
2022-11-26 22:24:58 +0000

    refactor(webpack): add collectAssets function

---
+++ webpack/assets.ts
@@ -0,0 +1,33 @@
+import type {Compilation} from "webpack";
+import {mapFn} from "@softwareventures/iterable";
+import {chain} from "@softwareventures/chain";
+import {notNull} from "@softwareventures/nullable";
+
+export interface Asset {
+    readonly immutable: boolean;
+    readonly buffer: () => Buffer;
+}
+
+export function collectAssets(compilation: Compilation): Map<string, Asset> {
+    return chain(compilation.assetsInfo)
+        .map(
+            mapFn(([key, info]) => ({
+                key,
+                asset: notNull(compilation.assets[key]),
+                info
+            }))
+        )
+        .map(
+            mapFn(
+                ({key, asset, info}) =>
+                    [
+                        key,
+                        {
+                            buffer: () => asset.buffer(),
+                            immutable: info.immutable ?? false
+                        }
+                    ] as const
+            )
+        )
+        .map(entries => new Map(entries)).value;
+}
